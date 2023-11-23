577c98c8db6f656efc45a12e7dc48c7dbbea2721
Ange230700 <kouakouangeericstephane@gmail.com>
2023-11-23 14:22:55 +0100

    pending...

--- src/components/ListCadeaux.jsx
+++ src/components/ListCadeaux.jsx
@@ -1,7 +1,171 @@
-import React from 'react'
+import Cadeaux from "./Cadeaux.jsx";
+import data from "../data.json";
+import PropTypes from "prop-types";
 
-export default function ListCadeaux() {
+function ListCadeaux({ id, Description, Category, Image }) {
   return (
-    <div>ListCadeaux</div>
-  )
+    <>
+      <section className="category-section">
+        <h2 className="category-title">Livres et films</h2>
+        {data
+          .filter((cadeau) => cadeau.Category === "Livres et films")
+          .map((cadeau) => {
+            return (
+              <div className="cadeau-container">
+                <Cadeaux
+                  key={cadeau.id}
+                  id={cadeau.id}
+                  Description={cadeau.Description}
+                  Category={cadeau.Category}
+                  Image={cadeau.Image}
+                  Onclick={handleClick}
+                />
+              </div>
+            );
+          })}
+      </section>
+      <section className="category-section">
+        <h2 className="category-title">Cosmétiques</h2>
+        {data
+          .filter((cadeau) => cadeau.Category === "cosmetique")
+          .map((cadeau) => {
+            return (
+              <div className="cadeau-container">
+                <Cadeaux
+                  key={cadeau.id}
+                  id={cadeau.id}
+                  Description={cadeau.Description}
+                  Category={cadeau.Category}
+                  Image={cadeau.Image}
+                  Onclick={handleClick}
+                />
+              </div>
+            );
+          })}
+      </section>
+      <section className="category-section">
+        <h2 className="category-title">Mode et accessoires</h2>
+        {data
+          .filter((cadeau) => cadeau.Category === "mode_accessoires")
+          .map((cadeau) => {
+            return (
+              <div className="cadeau-container">
+                <Cadeaux
+                  key={cadeau.id}
+                  id={cadeau.id}
+                  Description={cadeau.Description}
+                  Category={cadeau.Category}
+                  Image={cadeau.Image}
+                  Onclick={handleClick}
+                />
+              </div>
+            );
+          })}
+      </section>
+      <section className="category-section">
+        <h2 className="category-title">Electronique</h2>
+        {data
+          .filter((cadeau) => cadeau.Category === "electronique")
+          .map((cadeau) => {
+            return (
+              <div className="cadeau-container">
+                <Cadeaux
+                  key={cadeau.id}
+                  id={cadeau.id}
+                  Description={cadeau.Description}
+                  Category={cadeau.Category}
+                  Image={cadeau.Image}
+                  Onclick={handleClick}
+                />
+              </div>
+            );
+          })}
+      </section>
+      <section className="category-section">
+        <h2 className="category-title">Poupée</h2>
+        {data
+          .filter((cadeau) => cadeau.Category === "poupee")
+          .map((cadeau) => {
+            return (
+              <div className="cadeau-container">
+                <Cadeaux
+                  key={cadeau.id}
+                  id={cadeau.id}
+                  Description={cadeau.Description}
+                  Category={cadeau.Category}
+                  Image={cadeau.Image}
+                  Onclick={handleClick}
+                />
+              </div>
+            );
+          })}
+      </section>
+      <section className="category-section">
+        <h2 className="category-title">Jouet</h2>
+        {data
+          .filter((cadeau) => cadeau.Category === "jouet")
+          .map((cadeau) => {
+            return (
+              <div className="cadeau-container">
+                <Cadeaux
+                  key={cadeau.id}
+                  id={cadeau.id}
+                  Description={cadeau.Description}
+                  Category={cadeau.Category}
+                  Image={cadeau.Image}
+                  Onclick={handleClick}
+                />
+              </div>
+            );
+          })}
+      </section>
+      <section className="category-section">
+        <h2 className="category-title">Mode et beauté</h2>
+        {data
+          .filter((cadeau) => cadeau.Category === "Mode et beauté")
+          .map((cadeau) => {
+            return (
+              <div className="cadeau-container">
+                <Cadeaux
+                  key={cadeau.id}
+                  id={cadeau.id}
+                  Description={cadeau.Description}
+                  Category={cadeau.Category}
+                  Image={cadeau.Image}
+                  Onclick={handleClick}
+                />
+              </div>
+            );
+          })}
+      </section>
+      <section className="category-section">
+        <h2 className="category-title">Divers</h2>
+        {data
+          .filter((cadeau) => cadeau.Category === "Divers")
+          .map((cadeau) => {
+            return (
+              <div className="cadeau-container">
+                <Cadeaux
+                  key={cadeau.id}
+                  id={cadeau.id}
+                  Description={cadeau.Description}
+                  Category={cadeau.Category}
+                  Image={cadeau.Image}
+                  Onclick={handleClick}
+                />
+              </div>
+            );
+          })}
+      </section>
+    </>
+  );
 }
+
+ListCadeaux.propTypes = {
+  id: PropTypes.number,
+  Description: PropTypes.string,
+  Category: PropTypes.string,
+  Image: PropTypes.string,
+};
+
+export default ListCadeaux;
