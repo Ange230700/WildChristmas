8b87d76e1ea0ba6d5eee4ca353d6cc07752404aa
Karim Rejani <karim.rjn02@gmail.com>
2023-11-23 17:02:58 +0100

    fixing conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,6 @@ import { useState } from "react";
 import "./styles/index.scss";
 import Accueil from "./pages/Accueil";
 import "./styles/index.scss";
-<<<<<<< HEAD
-import Accueil from "./pages/Accueil";
-=======
->>>>>>> 4ae4326e73731867786c2e07891e82a24253445e
 
 function App() {
   return (
