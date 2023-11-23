ec912a5403ce9ffcd437b37eefeb8ee7f85945fa
Karim Rejani <karim.rjn02@gmail.com>
2023-11-23 14:04:16 +0100

    adding files..

---
+++ src/components/WishList.jsx
@@ -0,0 +1,14 @@
+// import { useState } from "react";
+
+// const Wishlist = () => {
+//   const [items, setItems] = useState([]);
+//   const [newItem, setNewItem] = useState("");
+
+//   return (
+//     <div>
+//       <h1>My Wishlist</h1>
+//     </div>
+//   );
+// };
+
+// export default Wishlist;
