4a66f1705dccbc1716fbeb3df5cc3ea301c4294f
Ange230700 <kouakouangeericstephane@gmail.com>
2023-11-24 11:16:08 +0100

    working on modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,4 +1,7 @@
-const Modal = ({ isModal, handleModal }) => {
+import PropTypes from "prop-types";
+import Cadeau from "./Cadeau";
+const Modal = ({ isModal, handleModal, arrayCadeaux }) => {
+  
   return (
     <div
       className={`containerModal ${
@@ -6,10 +9,38 @@ const Modal = ({ isModal, handleModal }) => {
       }`}
     >
       <div>
-        <button className="close" onClick={handleModal} >close</button>
+        <button className="close" onClick={handleModal}>
+          close
+        </button>
+      </div>
+      <div className="containerCadeaux">
+        {arrayCadeaux.map((cadeau) => (
+          <div key={cadeau.id} className="cadeau-container">
+            <Cadeau
+              id={cadeau.id}
+              Description={cadeau.Description}
+              Category={cadeau.Category}
+              Image={cadeau.Image}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
+};
+
+Modal.propTypes = {
+  isModal: PropTypes.bool.isRequired,
+  handleModal: PropTypes.func.isRequired,
+  arrayCadeaux: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      Description: PropTypes.string.isRequired,
+      Category: PropTypes.string.isRequired,
+      Image: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  setArrayCadeaux: PropTypes.func,
 };
 
 export default Modal;
--- src/components/WishList.jsx
+++ src/components/WishList.jsx
@@ -26,23 +26,27 @@ const Wishlist = ({ arrayCadeaux, setArrayCadeaux }) => {
     <>
       <div className="wishlist-container">
         <h2>Ma Wishlist</h2>
-        {arrayCadeaux.map((cadeau) => (
-          <button key={cadeau.id} onClick={() => handleClick(cadeau)}>
-            <div className="cadeau-container">
-              <Cadeau
-                id={cadeau.id}
-                Description={cadeau.Description}
-                Category={cadeau.Category}
-                Image={cadeau.Image}
-              />
-            </div>
-          </button>
-        ))}
+        {arrayCadeaux.length < 6 &&
+          arrayCadeaux.map((cadeau) => (
+            <button key={cadeau.id} onClick={() => handleClick(cadeau)}>
+              <div className="cadeau-container">
+                <Cadeau
+                  id={cadeau.id}
+                  Description={cadeau.Description}
+                  Category={cadeau.Category}
+                  Image={cadeau.Image}
+                />
+              </div>
+            </button>
+          ))}
+        {arrayCadeaux.length > 0 &&
+          arrayCadeaux.length >= 6 &&
+          arrayCadeaux.pop() && <p>Non non non, 5 cadeaux max !</p>}
         <div className="containerButton">
-          <button type="button" className="validButton" onClick={handleModal}>
+          <button type="button" className="validButton" onClick={() => handleModal()}>
             Validé
           </button>
-          <Modal handleModal={() => handleModal()} isModal={isModal} />
+          <Modal handleModal={handleModal} isModal={isModal} arrayCadeaux={arrayCadeaux} />
         </div>
       </div>
     </>
@@ -59,6 +63,8 @@ Wishlist.propTypes = {
     })
   ).isRequired,
   setArrayCadeaux: PropTypes.func,
+  isModal: PropTypes.bool.isRequired,
+  setIsModal: PropTypes.func.isRequired,
 };
 
 export default Wishlist;
